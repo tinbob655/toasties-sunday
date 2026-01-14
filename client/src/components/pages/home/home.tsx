ef76dc98e407d9849ca60e1eea0661a42c2abe3a
Will Rutland <wrutland07@gmail.com>
2026-01-14 16:29:09 +0000

    added all styles

--- client/src/components/pages/home/home.tsx
+++ client/src/components/pages/home/home.tsx
@@ -4,6 +4,18 @@ export default function Home():React.ReactElement {
 
     return (
         <React.Fragment>
+            <h1>
+                This is a header
+            </h1>
+            <h2>
+                This is a subheader
+            </h2>
+            <h3>
+                This is a button
+            </h3>
+            <p>
+                Incididunt officia id ea esse Lorem magna anim dolor qui id velit nisi voluptate.Exercitation et proident duis officia ex sint.
+            </p>
         </React.Fragment>
     );
 };
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router';
+import './scss/main.scss';
 
 import Header from './components/multiPageComponents/header';
 import AllRoutes from './routes';
