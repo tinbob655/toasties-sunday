c849a556dfd1f1f64f677b8935191fe917ca7334
Will Rutland <wrutland07@gmail.com>
2026-01-14 16:32:28 +0000

    added footer

--- client/src/components/multiPageComponents/footer.tsx
+++ client/src/components/multiPageComponents/footer.tsx
@@ -5,6 +5,12 @@ export default function Footer():React.ReactElement {
 
     return (
         <React.Fragment>
+            <div id="footer">
+                <img src="/logo.png" className="logo" style={{height: '400px'}} />
+                <p>
+                    Website created by <a href="https://github.com/tinbob655" style={{textDecoration: 'underline'}}>Tinbob655</a> for the Better Server community.
+                </p>
+            </div>
         </React.Fragment>
     );
 };
