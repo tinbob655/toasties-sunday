905a14dfbb5a6c8352913db418b98ad9c33317c6
Will Rutland <wrutland07@gmail.com>
2026-01-15 14:13:18 +0000

    undid vercel

--- server/index.js
+++ server/index.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
+app.use(cors());
 
 
 app.get('/api/hello', (req, res) => {
   res.json({ message: 'Hello from backend!' });
 });
+
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => console.log(`Website running on port ${PORT}`));
 
 module.exports = app;
