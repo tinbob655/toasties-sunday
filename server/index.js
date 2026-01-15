56c8a513cbc4f4503e632616e771bdb6d6b002da
Will Rutland <wrutland07@gmail.com>
2026-01-15 17:12:14 +0000

    setup for railway

--- server/index.js
+++ server/index.js
@@ -1,12 +1,24 @@
 const express = require('express');
-const app = express();
+const path = require('path');
 const cors = require('cors');
+const app = express();
 app.use(cors());
 
 
+
+//serve the frontend
+app.use(express.static(path.join(__dirname, '../client/dist')));
+
+//routes
 app.get('/api/hello', (req, res) => {
   res.json({ message: 'Hello from backend!' });
 });
+
+//fallback will give a page
+app.use((req, res) => {
+  res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+});
+
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log(`Website running on port ${PORT}`));
