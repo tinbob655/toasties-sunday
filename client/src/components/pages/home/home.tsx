2d913ee9d2a1f13d80cdbc08adae6eed6fbce3df
tinbob655 <wrutland07@gmail.com>
2026-01-17 10:45:34 +0000

    home page now responds if the user is logged in

--- client/src/components/pages/home/home.tsx
+++ client/src/components/pages/home/home.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import PageHeader from '../../multiPageComponents/pageHeader';
 import GenericTextSection from '../../multiPageComponents/genericTextSection';
+import { useAuth } from '../../../context/authContext';
 
 export default function Home():React.ReactElement {
+
+    const {loggedIn, username} = useAuth();
 
     return (
         <React.Fragment>
@@ -15,7 +18,19 @@ export default function Home():React.ReactElement {
             <GenericTextSection header="Place an order" paragraph="Place your order to get your sunday toasties!" linkDestination="/orders" linkText="Make an order" left={true} />
 
             {/*account section*/}
-            <GenericTextSection header="Log in / sign up" paragraph="In order to make orders you will need to be logged into an account." linkDestination="/account" linkText="Log in here" left={false} />
+            {loggedIn ? (
+                <React.Fragment>
+
+                    {/*the user is logged in*/}
+                    <GenericTextSection header={`Welcome back, ${username}`} paragraph="This would be a paragraph telling you to log in so you can make orders and stuff but you're already logged in so no need. Well done you!" linkDestination="/account" linkText="View your account here" left={false} />
+                </React.Fragment>
+            ) : (
+                <React.Fragment>
+
+                    {/*the user is not logged in*/}
+                    <GenericTextSection header="Log in / sign up" paragraph="In order to make orders you will need to be logged into an account." linkDestination="/account" linkText="Log in here" left={false} />
+                </React.Fragment>
+            )}
         </React.Fragment>
     );
 };
