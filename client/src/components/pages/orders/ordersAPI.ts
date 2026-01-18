ecac79486deafb748ca40941aaac4862dca2e383
tinbob655 <wrutland07@gmail.com>
2026-01-18 13:30:28 +0000

    added optional timecheck disable

--- client/src/components/pages/orders/ordersAPI.ts
+++ client/src/components/pages/orders/ordersAPI.ts
@@ -27,9 +27,11 @@ export async function getOrder(username: string): Promise<orderObj | null> {
 
 //place a new order
 export async function placeOrder(cost: number, username: string):Promise<orderObj> {
+
     //the user cannot make orders between 13:00 and 21:59 on a Sunday.
+    const DISABLE_TIMECHECK = true;
     const currentTime: Date = new Date();
-    if (false) {
+    if (!DISABLE_TIMECHECK && currentTime.getDay() === 0 && currentTime.getHours() >= 13 && currentTime.getHours() < 22) {
 
         //invalid time to make an order
         throw new Error("Orders cannot be placed between 1:00 PM and 9:59 PM on Sundays.");
